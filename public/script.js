63df3134a66c022891d984ac5351a12ecb912ab0
alexandrexd436@gmail.com <alexandrexd436@gmail.com>
2023-07-03 20:09:28 -0500

    Atualizando

---
+++ public/script.js
@@ -0,0 +1,85 @@
+const ul = document.querySelector('ul')
+const input = document.querySelector('input')
+const form = document.querySelector('form')
+
+/*
+
+// Não se preocupem com esse pedaço de código comentado! Vamos descomentá-lo quando tivermos acabado de construir a API.
+
+// Função que carrega o conteúdo da API.
+async function load() {
+    // fetch está como await para evitar que entre num esquema de promisse e só devolva o conteúdo após a iteração qua acontece em seguida.
+    const res = await fetch('http://localhost:3000/')
+        .then(data => data.json())
+    // Iterando no vetor com o conteúdo (JSON) que está vindo da API e adicionando-os no frontend.
+    res.urls.map(({name, url}) => addElement({name, url}))
+}
+
+load()
+*/
+
+const elements = [];
+
+function addElement({ name, url }) {
+    const li = document.createElement('li');
+    const trash =document.createElement('span')
+    const a = document.createElement('a')
+    a.href = url
+    a.target = "_blank"
+    li.innerHTML = `${name}`;
+    elements.push(li);
+    const respostas = document.getElementById('respostas');
+    respostas.innerHTML = '';
+    trash.innerHTML= "x"
+    trash.onclick= () => removeElement(trash)
+
+
+    elements.forEach((element) => {
+        respostas.appendChild(element);
+
+    })
+    li.append(a)
+    li.append(trash)
+
+}
+
+function removeElement(element) {
+    document.querySelector('#apagar')
+    apg.addEventListener('click', function(e){
+        e.preventDefault();
+        const respostas = document.getElementById('respostas');
+        const delet = document.getElementById('delete')
+        if (!delet) {
+            elements.remove(delet)
+        }
+        respostas.innerHTML = '';
+        elements.length = 0;
+    })
+
+
+}
+
+
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    let { value } = input
+
+    if (!value)
+        return alert('Preencha o campo!')
+
+    const [name, url] = value.split(',')
+
+    if (!url)
+        return alert('O texto não está formatado da maneira correta.')
+
+    if (!/^http/.test(url))
+        return alert('Digite a url da maneira correta.')
+
+    addElement({ name, url })
+
+    input.value = ''
+
+})
--- server.js
+++ server.js
@@ -2,13 +2,28 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 
-
 http.createServer((req,res)=>{
+
+    const file = (req.url === '/') ? 'index.html' : req.url
+    const pathFile = path.join(__dirname,'public', file)
+
+    const extname = path.extname(pathFile)
+    const allowedFileTypes = ['.html','.css','.js']
+    const allowed = allowedFileTypes.find((item) => item == extname)
+
+    if(!allowed) return //Em resumo, um break, não vai execultar mais nada abaixo.
+
+    fs.readFile(pathFile , (err,content) => {
+        if(err) throw err
+
+        res.end(content)
+
+    })
+    /*
     if (req.url === '/') 
-        fs.readFile(path.join(__dirname, 'public', 'index.html'),(err, content)=>{
-            if (err) throw err
-            res.end(content)
+
         })
+        */
 }).listen(5000,()=>{
-    //console.log('Servidor rodando')
+    console.log('Servidor rodando')
 })
